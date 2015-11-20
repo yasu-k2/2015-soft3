80cbfb74784321cbc496a1d71ca4e29dc06560ac
yasu-k2 <yasu.mech14@gmail.com>
2015-11-21 03:53:08 +0900

    remove unnecessary check files

--- 20151111/src/enshu_20151111/ExampleMain.java
+++
@@ -1,8 +0,0 @@
-public class ExampleMain {
-    public static void main(String[] args) {
-        System.loadLibrary("ffi-class_wrap");
-        Example e = new Example();
-        e.Hello("world");
-        e.Sinc(1.0);
-    }
-}
--- 20151111/src/enshu_20151111/ffi-c++-main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-double sinc(double x);
-
-int main () {
-    double f = 1.0;
-    fprintf(stderr, "sinc(%f) = %f\n", f, sinc(f));
-    return 0;
-}
