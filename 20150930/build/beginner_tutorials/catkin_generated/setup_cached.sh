55cf4d77b1322d60c71fb95b77118c1657e2c607
yasu-k2 <yasu.mech14@gmail.com>
2015-11-21 02:47:05 +0900

    before adding 20150930 files

---
+++ 20150930/build/beginner_tutorials/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yasu-k2/work/2015-soft3/20150930/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/yasu-k2/work/2015-soft3/20150930/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/yasu-k2/work/2015-soft3/20150930/devel/lib:/home/yasu-k2/work/2015-soft3/20150930/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/yasu-k2/work/2015-soft3/20150930/devel/bin:/opt/ros/indigo/bin:/home/yasu-k2/pylearn2/pylearn2/scripts:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/yasu-k2/work/2015-soft3/20150930/devel/lib/pkgconfig:/home/yasu-k2/work/2015-soft3/20150930/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PYTHONPATH="/home/yasu-k2/work/2015-soft3/20150930/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yasu-k2/work/2015-soft3/20150930/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/yasu-k2/work/2015-soft3/20150930/src/beginner_tutorials:$ROS_PACKAGE_PATH"
