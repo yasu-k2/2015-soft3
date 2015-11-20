587b934b5101f70eddf367e231b6fc040f41d175
yasu-k2 <yasu.mech14@gmail.com>
2015-11-21 03:06:59 +0900

    after removing unnecessary build files

--- 20150930/build/beginner_tutorials/CMakeFiles/clean_test_results_beginner_tutorials.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/clean_test_results_beginner_tutorials"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/clean_test_results_beginner_tutorials.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
