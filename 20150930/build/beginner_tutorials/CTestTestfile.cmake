9b8b251428b25aed36ae55f0018de4da7c53c9fc
yasu-k2 <yasu.mech14@gmail.com>
2015-12-04 05:06:35 +0900

    update origin

--- 20150930/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yasu-k2/work/2015-soft3/20150930/src/beginner_tutorials
-# Build directory: /home/yasu-k2/work/2015-soft3/20150930/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- 20150930/build/beginner_tutorials/catkin_generated/package.cmake
+++
@@ -1,7 +0,0 @@
-set(_CATKIN_CURRENT_PACKAGE "beginner_tutorials")
-set(beginner_tutorials_MAINTAINER "yasu-k2 <yasu-k2@todo.todo>")
-set(beginner_tutorials_DEPRECATED "")
-set(beginner_tutorials_VERSION "0.0.0")
-set(beginner_tutorials_BUILD_DEPENDS "geometry_msgs" "message_generation" "roscpp" "rospy" "std_msgs")
-set(beginner_tutorials_RUN_DEPENDS "geometry_msgs" "message_runtime" "roscpp" "rospy" "std_msgs")
-set(beginner_tutorials_BUILDTOOL_DEPENDS "catkin")
